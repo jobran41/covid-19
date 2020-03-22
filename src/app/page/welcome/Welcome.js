2aaa980f30128718daa2e91323894b4514e27d78
Jihed <gharsellaoui.jihed@gmail.com>
2020-03-22 18:00:57 +0100

    COVID19 WELCOME PAGE INTEGRATION

--- src/app/page/welcome/Welcome.js
+++ src/app/page/welcome/Welcome.js
@@ -1,7 +1,12 @@
 import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
 import { GroupedWelcomeCards } from "@fuse";
-
+import "../../scss/welcome_page.scss"
+import navbar from "../../store/reducers/fuse/navbar.reducer";
+import logo from "../../img/logo.svg"
+import facebook from "../../img/social/facebook-icon.svg"
+import instagram from "../../img/social/instagram-icon.svg"
+import twitter from "../../img/social/twitter-icon.svg"
 const styles = theme => ({
   layoutRoot: {
     paddingTop: "5rem",
@@ -16,13 +21,43 @@ class Welcome extends Component {
   render() {
     const { classes } = this.props;
     return (
-      <div className={classes.layoutRoot}>
-        <h1 className={classes.title}> Ensemble pour la tunisie</h1>
-        <div>
-          Une application qui va renforcer la connexion entre les citoyens et le
-          gouvernement{" "}
+      <div className="welcome-page">
+              <div className="main-navbar">
+                  <div className="logo-container">
+                      <img className="logo" src={logo} alt="logo"/>
+                  </div>
+                  <div className="social-container">
+                      <ul className="social-list">
+                          <li>
+                              <a href="#">
+                                  <img src={facebook} alt="facebook"/>
+                              </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                              <img src={instagram} alt="instagram"/>
+                          </a>
+                          </li>
+                          <li>
+                              <a href="#">
+                              <img src={twitter} alt="twitter"/>
+                          </a>
+                          </li>
+                      </ul>
+                  </div>
+                  {/*<div className="homepage">*/}
+                  {/*    <button className="btn">*/}
+                  {/*        go back*/}
+                  {/*    </button>*/}
+                  {/*</div>*/}
+          </div>
+        <h1 className="welcome-title"> Ensemble pour la Tunisie</h1>
+        <div className="welcome-subtitle">
+            Une application qui va renforcer la connection entre les citoyens, les docteurs et la gouvernorat <br/>pour combattre le virus Covid-19.
         </div>
-         <GroupedWelcomeCards/>
+         <div className="card-wrapper">
+             <GroupedWelcomeCards/>
+         </div>
       </div>
     );
   }
