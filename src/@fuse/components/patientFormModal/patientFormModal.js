bcc72d0969004cc42aeacbd98167c8f80e606f1f
jobran41 <jobran41@gmail.com>
2020-03-23 00:22:00 +0100

    add api to welcome

--- src/@fuse/components/patientFormModal/patientFormModal.js
+++ src/@fuse/components/patientFormModal/patientFormModal.js
@@ -27,182 +27,8 @@ import {
 import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import DateFnsUtils from "@date-io/date-fns";
 
-const helpers = [
-  {
-    section: "1- Questions Generales",
-    questions: [
-      {
-        id: 1,
-        fr_value: "Étiez vous à l'étranger récemment ?",
-        ar_value: "هل كنت  في الخارج مؤخرًا ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 2,
-        fr_value:
-          "Étiez vous en contact avec une personne venant de l'étranger ?",
-        ar_value: "هل كنت على اتصال بشخص قادم  من الخارج ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 3,
-        fr_value:
-          "Étiez en contact avec une personne qui a été testée positive ?",
-        ar_value: "هل كنت على اتصال بشخص كانت نتيجة اختباره إيجابية ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 4,
-        fr_value: "Quel est votre âge ?",
-        ar_value: "كم عمرك ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 5,
-        fr_value: "Êtes vous diabétique ? (oui, non)",
-        ar_value: "هل تعاني من مرض السكري ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 6,
-        fr_value:
-          "Avez vous une maladie respiratoire ? êtes vous suivi par un pneumologue ? (oui, non)",
-        ar_value: "هل تعاني من مرض تنفسي؟ هل يتابعك طبيب مختص في أمراض الرئة ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 7,
-        fr_value:
-          "Avez-vous de l'hypertension artérielle ? Ou avez-vous une maladie cardiaque ou vasculaire ? Ou prenez-vous un traitement à visée cardiologique ? (oui, non, je ne sais pas)",
-        ar_value:
-          "هل تعاني من ارتفاع ضغط الدم؟ أو لديك مرض القلب أو الأوعية الدموية؟ هل  تأخذ علاج للقلب ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 8,
-        fr_value:
-          "Avez-vous une insuffisance rénale chronique dialysée ? (oui, non)",
-        ar_value: "هل تعاني من قصور كلوي ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 9,
-        fr_value: "Avez-vous une maladie chronique de foie ? (oui, non)",
-        ar_value: "هل لديك مرض مزمن في الكبد ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 10,
-        fr_value: "Avez vous ou avez vous eu un cancer ? (oui, non)",
-        ar_value: "هل لديك أو كان  لديك مرض سرطان ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 11,
-        fr_value: "Êtes vous enceinte ?(oui, non, non applicable) ?",
-        ar_value: "هل أنت حامل ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 12,
-        fr_value:
-          "Avez-vous une maladie connue diminuer vos défenses immunitaires ? (oui, non)",
-        ar_value: "هل تعاني من مرض ينقص من مناعتك؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 13,
-        fr_value: "Prenez vous un traitement immunosuppresseur ? (oui, non)",
-        ar_value: "هل تتناول علاج مثبط للمناعة ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 14,
-        fr_value:
-          "Avez-vous de la fièvre, des frissons, des sueurs ? (oui, non)",
-        ar_value: "هل تعاني من الحمى والرعشة والتعرق ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 15,
-        fr_value: "Si oui indiquez la temperature ?",
-        ar_value: "إذا كان الجواب نعم ، حدد درجة الحرارة؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 16,
-        fr_value:
-          "Avez-vous une toux ou une augmentation de votre toux habituelle ces derniers jours ? (oui, non)",
-        ar_value:
-          "هل تعاني من السعال  أو زيادة في السعال المعتاد  في الأيام القليلة الماضية ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 17,
-        fr_value:
-          "Avez-vous un mal de gorge apparu ces derniers jours ? (oui, non)",
-        ar_value: "هل أصبت بالتهاب حنجرة  في الأيام الأخيرة ؟",
-        oui: false,
-        non: false
-      }
-    ]
-  },
-  {
-    section: "2- Section Médicale",
-    questions: [
-      {
-        id: 18,
-        fr_value: "Avez-vous des maux de tête ? (oui, non)",
-        ar_value: "هل تعاني من آلام بالرأس ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 19,
-        fr_value:
-          "Avez-vous des douleurs musculaires ou des courbatures inhabituelles ces derniers jours ? (oui, non)",
-        ar_value:
-          "هل عانيت من آلام أو آلام عضلية غير معتادة في الأيام القليلة الماضية ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 20,
-        fr_value:
-          "Avez-vous une fatigue inhabituelle ces derniers jours ? (oui, non)",
-        ar_value: "هل تعاني من تعب غير معتاد  في الأيام الأخيرة ؟",
-        oui: false,
-        non: false
-      },
-      {
-        id: 21,
-        fr_value:
-          "Avez-vous une gêne respiratoire ou une augmentation de votre gêne respiratoire inhabituelle ? (oui, non)",
-        ar_value: "هل  تعاني من صعوبة في التنفس ؟",
-        oui: false,
-        non: false
-      }
-    ]
-  }
-];
 
-const PatientFormModal = ({ modalAction,history }) => {
+const PatientFormModal = ({ modalAction,history,dataModal }) => {
   const handleClose = id => {
     modalAction(id);
   };
@@ -241,7 +67,7 @@ const PatientFormModal = ({ modalAction,history }) => {
         <button onClick={() => handleClose("PatientForm")}>x</button>
       </div>
       <div className="modal-content">
-      {helpers.map(el => {
+      {dataModal && dataModal.map(el => {
         return (
           <div className="question-list">
             <h4>{el.section}</h4>
@@ -387,7 +213,10 @@ const PatientFormModal = ({ modalAction,history }) => {
                   variant="outlined"
                   color="primary"
                   disabled={isSubmitting}
-                  onClick={resetForm}
+                  onClick={()=>{
+                    resetForm()
+                    handleClose("PatientForm")
+                  }}
                 >
                   Annuler
                 </Button>
