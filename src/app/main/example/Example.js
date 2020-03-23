fff6b6fd5aa206d5f7e71c7df714418da1585c91
jobran41 <jobran41@gmail.com>
2020-03-23 05:39:54 +0100

    add tunisia map

--- src/app/main/example/Example.js
+++ src/app/main/example/Example.js
@@ -1,56 +1,107 @@
-import React, { Component } from "react";
-import { withStyles } from "@material-ui/core/styles";
-import { FusePageSimple, DemoContent } from "@fuse";
+import React, { Component } from 'react'
+import Highmaps from 'highcharts/highmaps'
+import { Fetch } from 'react-request'
+import {
+  HighchartsMapChart, withHighmaps, Title, Subtitle, Tooltip, Legend, XAxis, YAxis, MapSeries, MapLineSeries, MapBubbleSeries, MapNavigation, Credits
+} from 'react-jsx-highmaps'
+/* import ExampleCode from '../utils/ExampleCode'
+import code from './exampleCode' */
 
-import QuestionEducation from "./QuestionEducation";
-import Form from "../example/form"
+import "./exemple.scss"
 
-const styles = theme => ({
-  layoutRoot: {}
-});
+Highmaps.setOptions({
+  lang: { thousandsSep: ',' }
+})
 
-class Example extends Component {
+class App extends Component {
+
   state = {
-    form: []
-  };
+    populationData: null
+  }
 
-  getAllState = data => {
-    const newForm = this.state.form;
-    newForm.push(data);
-    this.setState({ form: newForm });
-  };
-  render() {
-    const { classes } = this.props;
+  componentDidMount () {
+    fetch('https://cdn.rawgit.com/highcharts/highcharts/057b672172ccc6c08fe7dbb27fc17ebca3f5b770/samples/data/us-capitals.json')
+      .then(res => {
+        if (res.ok) return res.json()
+      })
+      .then(json => {
+        const populationData = json.map(
+          ({ population, ...rest }) => ({ ...rest, z: population })
+        )
+
+        this.setState({
+          populationData
+        })
+      })
+  }
+
+  render () {
+    const { populationData } = this.state
     return (
-      <FusePageSimple
-        classes={{
-          root: classes.layoutRoot
-        }}
-        header={
-          <div className="p-24">
-            <h4>Header</h4>
-          </div>
-        }
-        contentToolbar={
-          <div className="px-24">
-            <h4>Content Toolbar</h4>
-          </div>
-        }
-        content={
-          <div className="p-24">
-            <QuestionEducation
-              getState={this.getAllState}
-              title="Lorem ipsume"
-              description="bla bla bla"
-              withTextField
-              textField={"Quel est votre age"}
-            />
-            <Form />
-          </div>
-        }
-      />
-    );
+      <div style={{height:"100%"}} className="app">
+        <Fetch url="http://code.highcharts.com/mapdata/countries/tn/tn-all.geo.json">
+          {({ fetching, failed, data }) => {
+            if (fetching) return <div>Loading…</div>
+            if (failed) return <div>Failed to load map.</div>
+
+            if (data) {
+              return (
+                <HighchartsMapChart className="chartHeight" map={data}>
+                  <Title>Covid-19 Real Time info</Title>
+
+                  <Subtitle>Tunisia Capitals</Subtitle>
+
+                  <XAxis crosshair={{ snap: false }} />
+
+                  <YAxis crosshair={{ snap: false }}>
+                    <MapSeries
+                      mapData={data}
+                      borderColor="#606060"
+                      nullColor="rgba(200, 200, 200, 0.2)"
+                      showInLegend={false}
+                    />
+
+                    <MapLineSeries
+                      data={Highmaps.geojson(data, 'mapline')}
+                      color="gray"
+                      enableMouseTracking={false}
+                      showInLegend={false}
+                    />
+
+                    <MapBubbleSeries
+                      dataLabels={{
+                        enabled: true,
+                        format: '{point.capital}'
+                      }}
+                      color={Highmaps.defaultOptions.colors[0]}
+                      name="State Capitals"
+                      data={populationData}
+                      maxSize="30%"
+                    />
+                  </YAxis>
+
+                  <MapNavigation>
+                    <MapNavigation.ZoomIn />
+                    <MapNavigation.ZoomOut />
+                  </MapNavigation>
+
+                  <Tooltip pointFormat='{point.capital}, {point.parentState}: <b>{point.z}</b><br/>' />
+
+                  <Credits />
+
+                  <Legend />
+                </HighchartsMapChart>
+              )
+            }
+
+            return null
+          }}
+        </Fetch>
+
+      {/*   <ExampleCode name="MapBubble">{code}</ExampleCode> */}
+      </div>
+    )
   }
 }
 
-export default withStyles(styles, { withTheme: true })(Example);
+export default withHighmaps(App, Highmaps)
